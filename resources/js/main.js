728ab9b984824eb5c6ef3cc0e1b057ade2bc5da3
akarazzak20 <akarazzak.mr@gmail.com>
2020-07-01 01:33:58 +0600

    PSD to HTML
    Created My First Repository

---
+++ resources/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready (function(){
+	
+	//STICKY MENU
+	$(".js--services-section").waypoint(function(direction){
+		if(direction=="down"){
+		   $("nav").addClass("sticky");		   
+		   }else{
+		   $("nav").removeClass("sticky")
+		   }	
+	});
+	
+	// mixitup [PORTFOLIO SECTION]
+	var mixer=mixitup('.container')
+});
+
+
+
+function openNav() {
+	document.getElementById("myNav").style.width="100%";
+}
+
+function closeNav() {
+	document.getElementById("myNav").style.width="0%";
+}
